6554cdc61e9a954a824f5e2abfa5c8e62053f278
guilherme-romanholo <guilhermeromanholobofo@gmail.com>
2023-06-13 11:59:43 -0300

    interface bonita, comeco

--- src/main/java/clinica/medica/gui/TelaLogadaUI.java
+++ src/main/java/clinica/medica/gui/TelaLogadaUI.java
@@ -15,6 +15,7 @@ import static clinica.medica.gui.LoginUI.frame;
 public class TelaLogadaUI extends JFrame implements ActionListener {
     private String ultimaTela, telaAtual;
     private JPanel menuPanel;
+    private JPanel usuarioPanel;
     private JPanel contentPanel;
     private CardLayout cardLayout;
 
@@ -25,11 +26,18 @@ public class TelaLogadaUI extends JFrame implements ActionListener {
 
         menuPanel = new JPanel();
         menuPanel.setLayout(new BorderLayout());
-        menuPanel.setBackground(UIManager.getColor(new JButton()));
+        menuPanel.setBackground(Color.decode("#98f8c7"));
 
         JPanel menuButtonsPanel = botoesNavegacao(user);
+        menuButtonsPanel.setBackground(Color.decode("#98f8c7"));
 
-        menuPanel.add(menuButtonsPanel, BorderLayout.WEST);
+        menuPanel.add(menuButtonsPanel, BorderLayout.EAST);
+
+        JLabel nomeClinicaLabel = new JLabel("  Healthy");
+
+        menuPanel.add(nomeClinicaLabel, BorderLayout.WEST);
+
+        criaPainelInfos(user);
 
         contentPanel = new JPanel();
         cardLayout = new CardLayout();
@@ -38,20 +46,24 @@ public class TelaLogadaUI extends JFrame implements ActionListener {
         criaPaineisConteudo(user);
 
         Container container = getContentPane();
-        container.setLayout(new BorderLayout());
-        container.add(menuPanel, BorderLayout.NORTH);
-        container.add(contentPanel, BorderLayout.CENTER);
-        
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+        container.add(menuPanel);
+        container.add(usuarioPanel);
+        container.add(contentPanel);
+
         telaAtual = "Principal";
         cardLayout.show(contentPanel, "Principal");
     }
 
     public void actionPerformed(ActionEvent e) {
         String nomePainel = e.getActionCommand();
-        if (nomePainel.equals("Sair")){
-            dispose();
-            LoginUI.frame.setVisible(true);
-        }else if(nomePainel.equals("Voltar")){
+        if (nomePainel.equals("Sair")) {
+            int resp = JOptionPane.showOptionDialog(null, "Tem certeza que deseja sair?", "Sair", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, new Object[]{"Sim", "Não"}, "Sim");
+            if (resp == JOptionPane.YES_OPTION) {
+                dispose();
+                LoginUI.frame.setVisible(true);
+            }
+        } else if (nomePainel.equals("Voltar")) {
             cardLayout.show(contentPanel, ultimaTela);
             telaAtual = ultimaTela;
         } else {
@@ -61,10 +73,48 @@ public class TelaLogadaUI extends JFrame implements ActionListener {
         }
     }
 
+    public <T extends Usuario> void criaPainelInfos(T user) {
+        usuarioPanel = new JPanel();
+        usuarioPanel.setLayout(new GridBagLayout());
+        usuarioPanel.setBackground(Color.decode("#98f8c7"));
+
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        JLabel nome = new JLabel(user.getNome());
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        usuarioPanel.add(nome, constraints);
+
+        if (user instanceof Medico medico) {
+            JLabel crm = new JLabel(medico.getCRM());
+            JLabel atuacao = new JLabel(medico.getAreaAtuacao());
+
+            constraints.gridy = 1;
+            usuarioPanel.add(crm, constraints);
+
+            constraints.gridy = 2;
+            usuarioPanel.add(atuacao, constraints);
+        } else if (user instanceof Paciente paciente) {
+            JLabel cpf = new JLabel(paciente.getCpf());
+            JLabel idade = new JLabel(paciente.getIdade() + " anos");
+
+            constraints.gridy = 1;
+            usuarioPanel.add(cpf, constraints);
+
+            constraints.gridy = 2;
+            usuarioPanel.add(idade, constraints);
+        }
+    }
+
     public JButton criaBotaoMenu(String nome) {
         JButton button = new JButton(nome);
         button.setFocusPainted(false);
         button.setBorder(BorderFactory.createEmptyBorder(5, 10, 5, 10));
+        button.setBackground(Color.decode("#98f8c7"));
+        button.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        button.setFocusPainted(false);
+        button.setBorderPainted(false);
+        button.setContentAreaFilled(false);
         // Adicionar fonte, cores, etc ...
         return button;
     }
@@ -93,7 +143,7 @@ public class TelaLogadaUI extends JFrame implements ActionListener {
 
         buttons.add(criaBotaoMenu("Sair"));
 
-        for (JButton btn: buttons) {
+        for (JButton btn : buttons) {
             menuButtonsPanel.add(btn);
             btn.addActionListener(this);
         }
@@ -112,9 +162,9 @@ public class TelaLogadaUI extends JFrame implements ActionListener {
             contentPanel.add(TelaLogadaMedicoUI.telaAgendarConsulta(), "Consultas");
             contentPanel.add(CadastroUI.cadastroPaciente(), "Cadastrar paciente");
             contentPanel.add(TelaLogadaMedicoUI.telaNovoExame((Medico) user, this), "Prescrever novo exame");
-            contentPanel.add(TelaLogadaMedicoUI.showExames((Medico) user,this), "Verificar exames");
-            contentPanel.add(TelaLogadaMedicoUI.showExamesLaudo((Medico) user,this), "Prescrever novo laudo");
-            contentPanel.add(TelaLogadaMedicoUI.showLaudos((Medico) user,this), "Verificar laudos");
+            contentPanel.add(TelaLogadaMedicoUI.showExames((Medico) user, this), "Verificar exames");
+            contentPanel.add(TelaLogadaMedicoUI.showExamesLaudo((Medico) user, this), "Prescrever novo laudo");
+            contentPanel.add(TelaLogadaMedicoUI.showLaudos((Medico) user, this), "Verificar laudos");
             contentPanel.add(TelaLogadaMedicoUI.showPacientesReceita((Medico) user, this), "Prescrever nova receita");
             contentPanel.add(TelaLogadaMedicoUI.showReceitas((Medico) user, this), "Verificar receitas");
         } else if (user instanceof Paciente) {
@@ -129,7 +179,7 @@ public class TelaLogadaUI extends JFrame implements ActionListener {
         Component[] components = contentPanel.getComponents();
 
         for (Component comp : components) {
-                contentPanel.remove(comp);
+            contentPanel.remove(comp);
         }
 
         criaPaineisConteudo(user);
